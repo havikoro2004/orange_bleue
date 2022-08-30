be0ffa2f104620462ab03abfbd893e899b396876
flata <havikoro2004@gmail.com>
2022-08-30 12:33:03 +0200

    Config file .env.local / create database with DOCTRINE/install password hasher/create user Entoty/ sensio bundle

--- src/Controller/TesteController.php
+++ src/Controller/TesteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class TesteController extends AbstractController
 {
     #[Route('/teste', name: 'app_teste')]
+    #[Grantes]
     public function index(): Response
     {
+
         return $this->render('teste/index.html.twig', [
             'controller_name' => 'TesteController',
         ]);
