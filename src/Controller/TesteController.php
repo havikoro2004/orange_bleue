92a39fe6dc17ea34a5b41d28755e92b732a77edd
flata <havikoro2004@gmail.com>
2022-08-30 12:44:34 +0200

    Create admin auth

--- src/Controller/TesteController.php
+++ src/Controller/TesteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class TesteController extends AbstractController
 {
     #[Route('/teste', name: 'app_teste')]
-    #[Grantes]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
-
         return $this->render('teste/index.html.twig', [
             'controller_name' => 'TesteController',
         ]);
