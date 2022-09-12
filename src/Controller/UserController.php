08b691d696a4927169d34601036c5c76267db5d1
flata <havikoro2004@gmail.com>
2022-09-12 14:07:35 +0200

    Finir la page structure

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,31 +20,31 @@ class UserController extends AbstractController
 {
     #[Route('/client/user/add/{id}', name: 'app_user_client_add')]
     #[Entity('client', options: ['id' => 'id'])]
-    public function index(UserPasswordHasherInterface $hasher,UserRepository $UserRepository ,Client $client,ManagerRegistry $manager,Request $request,ValidatorInterface $validator): Response
+    public function index(UserPasswordHasherInterface $hasher, UserRepository $UserRepository, Client $client, ManagerRegistry $manager, Request $request, ValidatorInterface $validator): Response
     {
-        $users=null;
+        $users = null;
         $users = $UserRepository->findOneBy([
-           'client'=>$client->getId()
+            'client' => $client->getId()
         ]);
-        $user = New User();
+        $user = new User();
         $em = $manager->getManager();
         $error = null;
         $form = $this->createForm(UserType::class);
         $form->handleRequest($request);
         $data = $form->getData();
-        if ($data){
+        if ($data) {
             $error = $validator->validate($data);
         }
 
-        if ($form->isSubmitted() && $form->isValid()){
-            if ($form->get('password')->getData() !== $form->get('confirmPwd')->getData()){
-                $this->addFlash('alert','Les mots de passe ne correspondent pas');
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('password')->getData() !== $form->get('confirmPwd')->getData()) {
+                $this->addFlash('alert', 'Les mots de passe ne correspondent pas');
             } elseif ($UserRepository->findOneBy([
-                'email'=>$form->get('email')->getData()
-            ])){
-                $this->addFlash('alert','Cette adresse email est deja utilisé ');
-            }else {
-                $hachPwd = $hasher->hashPassword($user,$form->get('password')->getData());
+                'email' => $form->get('email')->getData()
+            ])) {
+                $this->addFlash('alert', 'Cette adresse email est deja utilisé ');
+            } else {
+                $hachPwd = $hasher->hashPassword($user, $form->get('password')->getData());
                 $data->setPassword($hachPwd);
                 $data->setConfirmPwd($hachPwd);
                 $data->setRoles(['ROLE_READER']);
@@ -52,97 +52,100 @@ class UserController extends AbstractController
                 $data->setCreateAt(new \DateTime('now'));
                 $em->persist($data);
                 $em->flush();
-                $this->addFlash('success','Le nouvel utilisateur a bien été enregistré');
-                return $this->redirectToRoute('app_user_client_add',[
-                    'id'=>$client->getId()
+                $this->addFlash('success', 'Le nouvel utilisateur a bien été enregistré');
+                return $this->redirectToRoute('app_user_client_add', [
+                    'id' => $client->getId()
                 ]);
             }
 
         }
 
         return $this->render('client/user/add.html.twig', [
-            'form'=>$form->createView(),
-            'errors'=>$error,'client'=>$client,
-            'user'=>$users
+            'form' => $form->createView(),
+            'errors' => $error, 'client' => $client,
+            'user' => $users
         ]);
     }
 
     #[Route('/client/{id_client}/user/{id_user}/edit')]
     #[Entity('client', options: ['id' => 'id_client'])]
-    public function edit(User $user,UserPasswordHasherInterface $hasher,UserRepository $UserRepository ,Client $client,ManagerRegistry $manager,Request $request,ValidatorInterface $validator):Response {
+    public function edit(User $user, UserPasswordHasherInterface $hasher, UserRepository $UserRepository, Client $client, ManagerRegistry $manager, Request $request, ValidatorInterface $validator): Response
+    {
         $em = $manager->getManager();
         $error = null;
-        $form= $this->createForm(UserType::class,$user);
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
         $data = $form->getData($user);
         $userExiste = $UserRepository->findOneBy([
-            'email'=>$form->get('email')->getData()
+            'email' => $form->get('email')->getData()
         ]);
-        if ($data){
+        if ($data) {
             $error = $validator->validate($data);
         }
-        if ($form->isSubmitted() && $form->isValid()){
-            if ($form->get('password')->getData() !== $form->get('confirmPwd')->getData()){
-                $this->addFlash('alert','Les mots de passe ne correspondent pas');
-            } elseif ($userExiste && $userExiste->getId() !== $user->getId()){
-                $this->addFlash('alert','Cette adresse email est deja utilisé ');
-            }else {
-                $hachPwd = $hasher->hashPassword($user,$form->get('password')->getData());
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('password')->getData() !== $form->get('confirmPwd')->getData()) {
+                $this->addFlash('alert', 'Les mots de passe ne correspondent pas');
+            } elseif ($userExiste && $userExiste->getId() !== $user->getId()) {
+                $this->addFlash('alert', 'Cette adresse email est deja utilisé ');
+            } else {
+                $hachPwd = $hasher->hashPassword($user, $form->get('password')->getData());
                 $data->setPassword($hachPwd);
                 $data->setConfirmPwd($hachPwd);
                 $data->setRoles(['ROLE_READER']);
                 $data->setClient($client);
                 $em->persist($data);
                 $em->flush();
-                $this->addFlash('success','L\'utilisateur a bien été modifié');
+                $this->addFlash('success', 'L\'utilisateur a bien été modifié');
             }
         }
 
         return $this->render('client/user/user_edit.html.twig', [
-            'form'=>$form->createView(),
-            'errors'=>$error,'client'=>$client,
+            'form' => $form->createView(),
+            'errors' => $error, 'client' => $client,
         ]);
     }
 
     #[Route('/client/{id_client}/user/{id_user}/delete')]
     #[Entity('client', options: ['id' => 'id_client'])]
-    public function delete(Client $client ,ManagerRegistry $manager,User $user):Response {
+    public function delete(Client $client, ManagerRegistry $manager, User $user): Response
+    {
         $em = $manager->getManager();
         $em->remove($user);
         $em->flush();
-        $this-> addFlash('success','L\'utilisateur a bien été supprimé');
+        $this->addFlash('success', 'L\'utilisateur a bien été supprimé');
 
-        return $this->redirectToRoute('app_user_client_add',[
-            'id'=>$client->getId()
+        return $this->redirectToRoute('app_user_client_add', [
+            'id' => $client->getId()
         ]);
     }
 
     #[Route('/client/{id_client}/branch/{id_branch}/add_user', name: 'app_user_branch_add')]
-    #[Entity('client',options: ['id'=>'id_client'])]
-    #[Entity('branch',options: ['id'=>'id_branch'])]
-    public function branchAddUser(UserPasswordHasherInterface $hasher,Branch $branch,ValidatorInterface $validator,UserRepository $UserRepository,Client $client,Request $request,ManagerRegistry $manager):Response {
-        $users=null;
+    #[Entity('client', options: ['id' => 'id_client'])]
+    #[Entity('branch', options: ['id' => 'id_branch'])]
+    public function branchAddUser(UserPasswordHasherInterface $hasher, Branch $branch, ValidatorInterface $validator, UserRepository $UserRepository, Client $client, Request $request, ManagerRegistry $manager): Response
+    {
+        $users = null;
         $users = $UserRepository->findOneBy([
-            'branch'=>$branch->getId()
+            'branch' => $branch->getId()
         ]);
-        $user = New User();
+        $user = new User();
         $em = $manager->getManager();
         $error = null;
         $form = $this->createForm(UserType::class);
         $form->handleRequest($request);
         $data = $form->getData();
-        if ($data){
+        if ($data) {
             $error = $validator->validate($data);
         }
-        if ($form->isSubmitted() && $form->isValid()){
-            if ($form->get('password')->getData() !== $form->get('confirmPwd')->getData()){
-                $this->addFlash('alert','Les mots de passe ne correspondent pas');
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('password')->getData() !== $form->get('confirmPwd')->getData()) {
+                $this->addFlash('alert', 'Les mots de passe ne correspondent pas');
             } elseif ($UserRepository->findOneBy([
-                'email'=>$form->get('email')->getData(),
-            ])){
-                $this->addFlash('alert','Cette adresse email est deja utilisé ');
-            }else {
-                $hachPwd = $hasher->hashPassword($user,$form->get('password')->getData());
+                'email' => $form->get('email')->getData(),
+            ])) {
+                $this->addFlash('alert', 'Cette adresse email est deja utilisé ');
+            } else {
+                $hachPwd = $hasher->hashPassword($user, $form->get('password')->getData());
                 $data->setPassword($hachPwd);
                 $data->setConfirmPwd($hachPwd);
                 $data->setRoles(['ROLE_USER']);
@@ -151,44 +154,95 @@ class UserController extends AbstractController
                 $em->persist($data);
                 $em->flush();
 
-                $this->addFlash('success','Le nouvel utilisateur a bien été enregistré');
-                return $this->redirectToRoute('app_user_branch_add',[
-                    'id_client'=>$client->getId(),
-                    'id_branch'=>$branch->getId()
+                $this->addFlash('success', 'Le nouvel utilisateur a bien été enregistré');
+                return $this->redirectToRoute('app_user_branch_add', [
+                    'id_client' => $client->getId(),
+                    'id_branch' => $branch->getId()
                 ]);
             }
 
         }
 
         return $this->render('branch/user/add.html.twig', [
-            'form'=>$form->createView(),
-            'client'=>$client,
-            'errors'=>$error,
-            'user'=>$users,
-            'branche'=>$branch
+            'form' => $form->createView(),
+            'client' => $client,
+            'errors' => $error,
+            'user' => $users,
+            'branche' => $branch
 
         ]);
     }
 
     #[Route('/client/{id_client}/branch/{id_branch}/delete_user')]
-    #[Entity('client',options: ['id'=>'id_client'])]
-    #[Entity('branch',options: ['id'=>'id_branch'])]
-    public function deleteUser(Branch $branch,UserRepository $userRepository,Client $client ,ManagerRegistry $manager):Response {
+    #[Entity('client', options: ['id' => 'id_client'])]
+    #[Entity('branch', options: ['id' => 'id_branch'])]
+    public function deleteUser(Branch $branch, UserRepository $userRepository, Client $client, ManagerRegistry $manager): Response
+    {
         $user = $userRepository->findOneBy([
-            'branch'=>$branch
+            'branch' => $branch
         ]);
         $em = $manager->getManager();
         $em->remove($user);
         $em->flush();
-        $this-> addFlash('success','L\'utilisateur de la structure a bien été supprimé');
-
-        $this->addFlash('success','Le nouvel utilisateur a bien été enregistré');
-        return $this->redirectToRoute('app_user_branch_add',[
-            'id_client'=>$client->getId(),
-            'id_branch'=>$branch->getId()
+        $this->addFlash('success', 'L\'utilisateur de la structure a bien été supprimé');
+        return $this->redirectToRoute('app_user_branch_add', [
+            'id_client' => $client->getId(),
+            'id_branch' => $branch->getId()
         ]);
     }
 
+    #[Route('/client/{id_client}/branch/{id_branch}/edit_user')]
+    #[Entity('client', options: ['id' => 'id_client'])]
+    #[Entity('branch', options: ['id' => 'id_branch'])]
+    public function branchUserEdit(UserPasswordHasherInterface $hasher, Branch $branch, ValidatorInterface $validator, UserRepository $UserRepository, Client $client, Request $request, ManagerRegistry $manager): Response
+    {
+        $users = null;
+        $users = $UserRepository->findOneBy([
+            'branch' => $branch->getId()
+        ]);
+        $user = new User();
+        $em = $manager->getManager();
+        $error = null;
+        $form = $this->createForm(UserType::class,$users);
+        $form->handleRequest($request);
+        $userExiste = $UserRepository->findOneBy([
+            'email' => $form->get('email')->getData()
+        ]);
+        $data = $form->getData();
+        if ($data) {
+            $error = $validator->validate($data);
+        }
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('password')->getData() !== $form->get('confirmPwd')->getData()) {
+                $this->addFlash('alert', 'Les mots de passe ne correspondent pas');
+            } elseif ($userExiste && $userExiste->getId() !== $users->getId()) {
+                $this->addFlash('alert', 'Cette adresse email est deja utilisé ');
+            } else {
+                $hachPwd = $hasher->hashPassword($user, $form->get('password')->getData());
+                $data->setPassword($hachPwd);
+                $data->setConfirmPwd($hachPwd);
+                $data->setRoles(['ROLE_USER']);
+                $data->setBranch($branch);
+                $data->setCreateAt(new \DateTime('now'));
+                $em->persist($data);
+                $em->flush();
 
+                $this->addFlash('success', 'L\'utilisateur de la structure a bien été modifié');
+                return $this->redirectToRoute('app_user_branch_add', [
+                    'id_client' => $client->getId(),
+                    'id_branch' => $branch->getId()
+                ]);
+            }
 
+        }
+
+        return $this->render('branch/user/user_edit.html.twig', [
+            'form' => $form->createView(),
+            'client' => $client,
+            'errors' => $error,
+            'user' => $users,
+            'branche' => $branch
+
+        ]);
+    }
 }
