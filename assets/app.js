ec318f0a12c920118eb9e811f5faa4f07d904082
flata <havikoro2004@gmail.com>
2022-09-26 01:28:57 +0200

    Fix conflic

--- assets/app.js
+++ assets/app.js
@@ -12,9 +12,7 @@ window.onpageshow = function(event) {
     if (event.persisted || performance.getEntriesByType("navigation")[0].type === 'back_forward') {
         location.reload();
     }
-}
-
-// Activer ou désactiver les clients dans la liste de la page d'accueil
+};
 const cardClient = document.getElementById('clientCard')
 if (cardClient){
     for (let i = 0 ; i < cardClient.children.length ; i++){
@@ -41,8 +39,6 @@ if (cardClient){
         })
     }
 }
-
-// Activer désactiver le client dans sa page profil
 const pageOneInput = document.getElementById('activBtnOnePage')
 if (pageOneInput){
     const btnActiveOnePage = pageOneInput.children[0]
@@ -65,8 +61,6 @@ if (pageOneInput){
         })
     })
 }
-
-//Activer ou désactiver les permissions globales d'un client dans sa page profil
 if (document.getElementById('permissionCollaps')){
     const permissionCollaps = document.getElementById('permissionCollaps')
     const switchesBtns = permissionCollaps.getElementsByClassName('form-check-input')
@@ -93,37 +87,34 @@ if (document.getElementById('permissionCollaps')){
         })
     }
 }
-
-// Activer ou désactiver une branche d'un client dans sa page profil
 if (document.getElementById('branchCard')){
     const activeBranchModal = document.getElementById('branchCard')
     for (let i=0 ; i< activeBranchModal.children.length ; i++){
-       const activeBtn = activeBranchModal.children[i].getElementsByClassName('activeBranch')
-            activeBtn[0].addEventListener('click',(e)=>{
-                console.log(activeBtn)
-                e.preventDefault()
-                const footer = activeBranchModal.children[i].getElementsByClassName('modal-footer')[0].children[1]
-                const body = activeBranchModal.children[i].getElementsByClassName('modal-body')[0]
-                footer.addEventListener('click',()=>{
-                    body.innerHTML="<div class=\"spinner-border text-primary text-center\" role=\"status\">\n" +
-                        "</div>"
-                    axios.post('/branch/'+ activeBtn[0].name+'/edit', {
-                    })
-                        .then(function (response) {
-                            console.log(response);
-                            document.body.scrollTop = document.documentElement.scrollTop = 0;
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                    location.reload();
+
+        const activeBtn = activeBranchModal.children[i].getElementsByClassName('activeBranch')
+        activeBtn[0].addEventListener('click',(e)=>{
+            e.preventDefault()
+            const footer = activeBranchModal.children[i].getElementsByClassName('modal-footer')[0].children[1]
+            const body = activeBranchModal.children[i].getElementsByClassName('modal-body')[0]
+            footer.addEventListener('click',()=>{
+                body.innerHTML="<div class=\"spinner-border text-primary text-center\" role=\"status\">\n" +
+                    "</div>"
+                axios.post('/branch/'+ activeBtn[0].name+'/edit', {
                 })
+                    .then(function (response) {
+                        console.log(response);
+                        document.body.scrollTop = document.documentElement.scrollTop = 0;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                location.reload();
             })
+        })
     }
 
 }
 
-// Activer ou désactiver des permissions d'une branche dans la page profil d'un client
 if(document.getElementById('branchCard')){
     const branchCard = document.getElementById('branchCard')
     for (let i=0;i<branchCard.children.length;i++){
@@ -155,54 +146,21 @@ if(document.getElementById('branchCard')){
     }
 }
 
-// Mettre l'input email d'un client en mode reaOnly pour ne pas pouvoir le modifier
 if (document.getElementById('modifClient')){
     const sectionPartenaireEdit = document.getElementById('modifClient')
     sectionPartenaireEdit.getElementsByTagName('form')[0].children[6].children[1].readOnly=true
 }
-// Mettre l'input email d'une structure en mode reaOnly pour ne pas pouvoir le modifier
+
 if (document.getElementById('editBranch')){
     const sectionBranchEdit =document.getElementById('editBranch')
     sectionBranchEdit.getElementsByTagName('form')[0].children[2].readOnly=true
 }
 
-// Modifier le design de la div erreur de type 'Alert' dans le champs modifier mot de passe
 if (document.getElementById('errorsNewPassword')){
     const errorsNewPassword = document.getElementById('errorsNewPassword')
     errorsNewPassword.getElementsByTagName('ul')[0].setAttribute('class','list-unstyled m-0 p-0')
 }
 
-<<<<<<< HEAD
-// Afficher les clients actifs seulement
-if (document.getElementById('actifs')){
-    var actifs = document.getElementById('actifs')
-    actifs.addEventListener('click',()=>{
-        actifs.value = 1
-        inactifs.value=0
-        tous.value=0
-        axios.get('/find_actifs', {
-            headers : {
-                'X-Requested-With' : 'XMLHttpRequest'
-            }
-        })
-            .then(function (response) {
-                document.getElementById('ajaxContent').innerHTML=response.data.ajaxContent
-                document.getElementById('paginator').innerHTML=response.data.paginator
-            })
-    })
-}
-
-// Afficher les clients inactifs seulement
-if (document.getElementById('inactifs')){
-    var inactifs = document.getElementById('inactifs')
-    inactifs.addEventListener('click',()=>{
-        actifs.value = 0
-        inactifs.value=1
-        tous.value=0
-        axios.get('/find_inactif', {
-            headers : {
-                'X-Requested-With' : 'XMLHttpRequest'
-=======
 // Pagination Script
 let nbrPage = 2
 const plusBtn = document.getElementById('plusBtn')
@@ -280,7 +238,6 @@ inactifCheckbox.addEventListener('click',()=>{
                 arrayFindByLetter.push(contentDiv.children[i])
             } else {
                 contentDiv.children[i].setAttribute('class','d-none')
->>>>>>> fixBugAjax
             }
         }
         if (arrayFindByLetter.length===0){
@@ -289,20 +246,6 @@ inactifCheckbox.addEventListener('click',()=>{
             alertNotFoundFilter.appendChild(alert)
         }
     })
-<<<<<<< HEAD
-}
-
-// Afficher tous les clients
-if (document.getElementById('tous')){
-    var tous = document.getElementById('tous')
-    tous.addEventListener('click',()=>{
-        actifs.value = 0
-        inactifs.value=0
-        tous.value=1
-        axios.get('/', {
-            headers : {
-                'X-Requested-With' : 'XMLHttpRequest'
-=======
 })
 
 // Afficher les partenaires actifs
@@ -346,7 +289,6 @@ actifsCheckbox.addEventListener('click',()=>{
                 arrayFindByLetter.push(contentDiv.children[i])
             } else {
                 contentDiv.children[i].setAttribute('class','d-none')
->>>>>>> fixBugAjax
             }
         }
         if (arrayFindByLetter.length===0){
@@ -355,19 +297,6 @@ actifsCheckbox.addEventListener('click',()=>{
             alertNotFoundFilter.appendChild(alert)
         }
     })
-<<<<<<< HEAD
-}
-
-// Afficher le client en tapant son nom sur le formulaire de recherche
-if (document.getElementById('recherche')){
-    const recherche = document.getElementById('recherche')
-    recherche.addEventListener('keyup',()=>{
-        var filterStatus = null
-        if (actifs.value==1){
-            filterStatus='actifs'
-        } else if (inactifs.value==1){
-            filterStatus='inactifs'
-=======
 
 })
 
@@ -378,7 +307,6 @@ plusBtn.addEventListener('click',()=>{
     arrayContent.forEach(item=>{
         if (arraySliced.includes(item)){
             item.setAttribute('class','my-3 p-4 article m-auto rounded list-group')
->>>>>>> fixBugAjax
         } else {
             item.setAttribute('class','d-none')
         }
@@ -418,55 +346,4 @@ const finByLetter = function(){
         }
     })
 }
-<<<<<<< HEAD
-
-// Afficher les branches activent seulement
-if (document.getElementById('branchActifs')){
-    var branchActifs = document.getElementById('branchActifs')
-    branchActifs.addEventListener('click',()=>{
-        axios.post('/branch_actifs', {
-            idClient: branchActifs.name,
-        })
-            .then(function (response) {
-                document.getElementById('branchCard').innerHTML=response.data.branchCard
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    })
-}
-
-// Afficher les branches inactives seulement
-if (document.getElementById('branchInactifs')){
-    var branchInactifs = document.getElementById('branchInactifs')
-    branchInactifs.addEventListener('click',()=>{
-        axios.post('/branch_inactifs', {
-            idClient: branchInactifs.name,
-        })
-            .then(function (response) {
-                document.getElementById('branchCard').innerHTML=response.data.branchCard
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    })
-}
-
-// Afficher toutes les branches
-if (document.getElementById('branchTous')){
-    var branchTous = document.getElementById('branchTous')
-    branchTous.addEventListener('click',()=>{
-        axios.post('/branch_tous', {
-            idClient: branchTous.name,
-        })
-            .then(function (response) {
-                document.getElementById('branchCard').innerHTML=response.data.branchCard
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    })
-}
-=======
 finByLetter()
->>>>>>> fixBugAjax
